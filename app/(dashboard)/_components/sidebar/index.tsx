4e0a1b8cca6dcc316042ce7df3365bae237a0846
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-02-24 19:54:11 +0530

    added create organization functionality in sideBar component

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button'
+import { Ghost, Plus } from 'lucide-react'
 import React from 'react'
+import { NewButton } from './new-button'
+import { List } from './list'
 
 const SideBar = () => {
   return (
-    <aside className='fixed z-[1] left-0 bg-blue-950 h-full w-[60px] felx p-3 flex-col gap-y-4 text-white'>
-      Side
+    <aside className='fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white'>
+      <List/>
+      <NewButton/>
     </aside>
   )
 }
