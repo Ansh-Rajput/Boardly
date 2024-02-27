dd1c4f5e57d3dff50ecdeadfd6d641709fb0b026
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-02-27 21:12:50 +0530

    created basic types and functionality for toolbar component of canvas

--- app/board/_components/canvas.tsx
+++ app/board/_components/canvas.tsx
@@ -1,23 +1,38 @@
 "use client";
 
-import { useSelf } from "@/liveblocks.config";
+import { useCanRedo, useCanUndo, useHistory, useSelf } from "@/liveblocks.config";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { ToolBar } from "./toolbar";
+import { CanvasMode, CanvasState } from "@/types/canvas";
+import { useState } from "react";
 
 interface CanvasProps {
     boardId:string;
 };
 
 export const Canvas = ({boardId}:CanvasProps) => {
-    
+    const [canvasState,setCanvasState] = useState<CanvasState>({
+        mode:CanvasMode.None,
+    });    
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
     return (
         <main
         className='h-full w-full relative bg-neutral-100 touch-none'
         >
             <Info boardId={boardId}/>
             <Participants/>
-            <ToolBar/>
+            <ToolBar 
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                undo={history.undo}
+                redo={history.redo}
+            />
         </main>
       )
 }
