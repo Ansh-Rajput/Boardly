207f6b40c7d53e2ff0cedcc3412bfed07a36717e
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-02-29 13:21:02 +0530

    created selectionTools component to change color or delete a layer on canves

--- app/board/_components/canvas.tsx
+++ app/board/_components/canvas.tsx
@@ -29,6 +29,7 @@ import { connectionIdToColor, pointerEventToCanvasPoint, resizeBounds } from "@/
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 
@@ -305,6 +306,10 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         canUndo={canUndo}
         undo={history.undo}
         redo={history.redo}
+      />
+      <SelectionTools
+        camera={camera}
+        setLastUsedColor={setLastUsedColor}
       />
       <svg
         className="h-[100vh] w-[100vw]"
